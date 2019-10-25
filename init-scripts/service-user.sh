cdd9231989bad488ba9f68a2e5bf9ec4cdebf4c0
Knut Magne Riise <kriise@Knuts-MacBook-Pro.local>
2019-10-25 13:55:35 +0200

    more changes to try to fix paths for srv-user credentials, for science

--- init-scripts/service-user.sh
+++ init-scripts/service-user.sh
@@ -2,13 +2,13 @@
 
 if test -f /secrets/serviceuser/srvdp-kalkulator-api/username;
 then
-    export  STS_USERNAME=$(cat /secrets/serviceuser/srvdp-graphql/username)
+    export  STS_USERNAME=$(cat /secrets/serviceuser/srvdp-kalkulator-api/username)
     echo "exported STS_USERNAME"
 else
    echo "SECRETS not mounted"
 fi
 if test -f /secrets/serviceuser/srvdp-kalkulator-api/password;
 then
-    export  STS_PASSWORD=$(cat /secrets/serviceuser/srvdp-graphql/password)
+    export  STS_PASSWORD=$(cat /secrets/serviceuser/srvdp-kalkulator-api/password)
     echo "exported STS_PASSWORD"
 fi
