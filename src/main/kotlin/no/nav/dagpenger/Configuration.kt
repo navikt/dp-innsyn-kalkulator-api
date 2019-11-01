cde03cae7ea76e708ebf330167b08b4d0898eb06
Knut Magne Riise <kriise@Knuts-MacBook-Pro.local>
2019-11-01 10:55:12 +0100

    navikt/dagpenger#238 tried fixing discovery url for dev

--- src/main/kotlin/no/nav/dagpenger/Configuration.kt
+++ src/main/kotlin/no/nav/dagpenger/Configuration.kt
@@ -21,7 +21,7 @@ private val localProperties = ConfigurationMap(
 )
 private val devProperties = ConfigurationMap(
     mapOf(
-            "jwks.url" to "https://login.microsoftonline.com/navtestb2c.onmicrosoft.com/discovery/v2.0/keys?p=b2c_1a_idporten",
+            "jwks.url" to "https://login.microsoftonline.com/navtestb2c.onmicrosoft.com/discovery/v2.0/keys?p=b2c_1a_idporten_ver1",
             "jwks.issuer" to "https://login.microsoftonline.com/d38f25aa-eab8-4c50-9f28-ebf92c1256f2/v2.0/",
         "application.profile" to "DEV",
         "application.httpPort" to "8099",
