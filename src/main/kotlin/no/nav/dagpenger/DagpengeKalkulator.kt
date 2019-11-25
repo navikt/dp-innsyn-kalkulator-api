c769c7d2ff02fca7fe40c01081a6225d6741c369
Andreas Bergh <andreas.nygard.bergh@nav.no>
2019-11-25 14:20:05 +0100

    Legg til måling for minsteinntek

--- src/main/kotlin/no/nav/dagpenger/DagpengeKalkulator.kt
+++ src/main/kotlin/no/nav/dagpenger/DagpengeKalkulator.kt
@@ -1,10 +1,18 @@
 package no.nav.dagpenger
 
+import io.prometheus.client.Counter
 import mu.KotlinLogging
 import no.nav.dagpenger.regel.api.internal.BehovStatusPoller
 import no.nav.dagpenger.regel.api.internal.SubsumsjonFetcher
 
 private val LOGGER = KotlinLogging.logger {}
+
+private val resultatCounter = Counter
+    .build()
+    .name("kalkulator_minsteinntekt")
+    .help("Oppfyller krav til minsteinntekt fra kalkulator")
+    .labelNames("resultat")
+    .register()
 
 class DagpengeKalkulator(
     val behovStarter: BehovStarter,
@@ -21,10 +29,22 @@ class DagpengeKalkulator(
 
         val subsumsjon = subsumsjonFetcher.getSubsumsjon(subsumsjonLocation)
 
+        val oppfyllerMinsteinntekt = subsumsjon.minsteinntektResultat?.oppfyllerMinsteinntekt
+            ?: throw IncompleteResultException("Missing minsteinntektResultat")
+
+        val periode =
+            subsumsjon.periodeResultat?.periodeAntallUker ?: throw IncompleteResultException("Missing periodeResultat")
+
+        if (oppfyllerMinsteinntekt) {
+            resultatCounter.labels("true").inc()
+        } else {
+            resultatCounter.labels("false").inc()
+        }
+
         return KalkulasjonsResult(
-                subsumsjon.minsteinntektResultat?.oppfyllerMinsteinntekt ?: throw IncompleteResultException("Missing minsteinntektResultat"),
-                subsumsjon.satsResultat?.ukesats ?: throw IncompleteResultException("Missing satsResultat"),
-                subsumsjon.periodeResultat?.periodeAntallUker ?: throw IncompleteResultException("Missing periodeResultat")
+            oppfyllerMinsteinntekt,
+            subsumsjon.satsResultat?.ukesats ?: throw IncompleteResultException("Missing satsResultat"),
+            periode
         )
     }
 }
