73d8ad7950fe993c2d4f138c9d61f4a10410447c
Geir André Lund <geir.andre.lund@nav.no>
2020-10-28 12:59:54 +0100

    Test new jwks url

--- src/main/kotlin/no/nav/dagpenger/kalkulator/Configuration.kt
+++ src/main/kotlin/no/nav/dagpenger/kalkulator/Configuration.kt
@@ -25,7 +25,7 @@ private val localProperties = ConfigurationMap(
 )
 private val devProperties = ConfigurationMap(
     mapOf(
-        "jwks.url" to System.getenv("LOGINSERVICE_IDPORTEN_DISCOVERY_URL"),
+        "jwks.url" to "https://navtestb2c.b2clogin.com/navtestb2c.onmicrosoft.com/discovery/v2.0/keys?p=b2c_1a_idporten_ver1",
         "jwks.issuer" to "https://navtestb2c.b2clogin.com/d38f25aa-eab8-4c50-9f28-ebf92c1256f2/v2.0/",
         "application.profile" to "DEV",
         "application.httpPort" to "8099",
