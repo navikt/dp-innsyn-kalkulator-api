89dc07f79971466d1b52f36c7af7b067c9233f58
Geir André Lund <geir.andre.lund@nav.no>
2020-10-28 10:02:08 +0100

    Tester ny innlogging. Issuer fra JWK provider Tok vekk mulighet for prod deploy temporært Co-authored-by: Knut Magne Riise <knut.magne.riise@nav.no>

--- src/main/kotlin/no/nav/dagpenger/kalkulator/KalkulatorApp.kt
+++ src/main/kotlin/no/nav/dagpenger/kalkulator/KalkulatorApp.kt
@@ -101,7 +101,7 @@ fun Application.KalkulatorApi(
 
     install(Authentication) {
         jwt {
-            verifier(jwkProvider, jwtIssuer)
+            verifier(jwkProvider)
             realm = "dp-kalkulator-api"
             authHeader {
                 val cookie = it.request.cookies["selvbetjening-idtoken"]
