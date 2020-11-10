b01f1d2bf8b6ef5f33d7b7804b876fe4ffb472de
André Roaldseth <andre.roaldseth@nav.no>
2020-11-10 09:30:32 +0100

    Bruk ConfigMap for loginservice URLs i preprod
    Issue navikt/dagpenger#593
    
    Co-authored-by: Tonje Mjøvik Myran <tonje.mjovik.myran@nav.no>

--- src/main/kotlin/no/nav/dagpenger/kalkulator/Configuration.kt
+++ src/main/kotlin/no/nav/dagpenger/kalkulator/Configuration.kt
@@ -13,8 +13,8 @@ private val localProperties = ConfigurationMap(
     mapOf(
         "application.profile" to "LOCAL",
         "application.httpPort" to "8099",
-        "jwks.url" to "https://localhost",
-        "jwks.issuer" to "https://localhost",
+        "loginservice.idporten.discovery.url" to "https://localhost",
+        "loginservice.idporten.audience" to "https://localhost",
         "API_GATEWAY_API_KEY" to "hunter2",
         "API_GATEWAY_URL" to "http://localhost/",
         "regel.api.secret" to "supersecret",
@@ -25,8 +25,6 @@ private val localProperties = ConfigurationMap(
 )
 private val devProperties = ConfigurationMap(
     mapOf(
-        "jwks.url" to "https://navtestb2c.b2clogin.com/navtestb2c.onmicrosoft.com/discovery/v2.0/keys?p=b2c_1a_idporten_ver1",
-        "jwks.issuer" to "https://navtestb2c.b2clogin.com/d38f25aa-eab8-4c50-9f28-ebf92c1256f2/v2.0/",
         "application.profile" to "DEV",
         "application.httpPort" to "8099",
         "API_GATEWAY_URL" to "https://api-gw-q1.oera.no/dp-reverse-proxy/"
@@ -34,8 +32,8 @@ private val devProperties = ConfigurationMap(
 )
 private val prodProperties = ConfigurationMap(
     mapOf(
-        "jwks.url" to "https://login.microsoftonline.com/navnob2c.onmicrosoft.com/discovery/v2.0/keys?p=b2c_1a_idporten",
-        "jwks.issuer" to "https://login.microsoftonline.com/8b7dfc8b-b52e-4741-bde4-d83ea366f94f/v2.0/",
+        "loginservice.idporten.discovery.url" to "https://login.microsoftonline.com/navnob2c.onmicrosoft.com/discovery/v2.0/keys?p=b2c_1a_idporten",
+        "loginservice.idporten.audience" to "https://login.microsoftonline.com/8b7dfc8b-b52e-4741-bde4-d83ea366f94f/v2.0/",
         "application.profile" to "PROD",
         "application.httpPort" to "8099",
         "API_GATEWAY_URL" to "https://api-gw.oera.no/dp-reverse-proxy/"
@@ -43,10 +41,8 @@ private val prodProperties = ConfigurationMap(
 )
 
 data class Configuration(
-
     val application: Application = Application(),
     val auth: Auth = Auth()
-
 ) {
     class Auth(
         regelApiSecret: String = config()[Key("regel.api.secret", stringType)],
@@ -58,8 +54,8 @@ data class Configuration(
     data class Application(
         val profile: Profile = config()[Key("application.profile", stringType)].let { Profile.valueOf(it) },
         val httpPort: Int = config()[Key("application.httpPort", intType)],
-        val jwksUrl: String = config()[Key("jwks.url", stringType)],
-        val jwksIssuer: String = config()[Key("jwks.issuer", stringType)],
+        val jwksUrl: String = config()[Key("loginservice.idporten.discovery.url", stringType)],
+        val jwksIssuer: String = config()[Key("loginservice.idporten.audience", stringType)],
         val name: String = "dp-kalkulator-api",
         val apiGatewayBaseUrl: String = config()[Key("API_GATEWAY_URL", stringType)],
         val apiGatewayKey: String = config()[Key("API_GATEWAY_API_KEY", stringType)],
