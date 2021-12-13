45a9bd4d916c7e523b91af6309ecf836e3f48c61
Geir André Lund <geir.andre.lund@nav.no>
2021-12-13 10:05:55 +0100

    log4j2-logstash-layout er ikke vedlikeholdt lenger - bytter til fordel for JsonTemplateLayout fra log4j2 https://logging.apache.org/log4j/2.x/manual/json-template-layout.html

--- build.gradle.kts
+++ build.gradle.kts
@@ -46,7 +46,7 @@ dependencies {
     implementation(Log4j2.api)
     implementation(Log4j2.core)
     implementation(Log4j2.slf4j)
-    implementation(Log4j2.Logstash.logstashLayout)
+    implementation(Log4j2.library("layout-template-json"))
     implementation(Kotlin.Logging.kotlinLogging)
 
     implementation(Dagpenger.Biblioteker.stsKlient)
